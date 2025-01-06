825cc2b75fa13dd8d88200f313fbe883acabe4ad
Artem Kostiuk <kostiuk.ua@gmail.com>
2025-01-06 12:48:51 +0800

    Update MyFirstProgram.java

--- sandbox/src/Main/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/Main/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,6 +2,25 @@ package Main.stqa.pft.sandbox;
 
 public class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        hello("Artem");
+        hello("Snega");
+        hello("Oleg ");
+        double var = 5.0;
+        double l = 3.0;
+        double s = 16.0;
+        System.out.println("Площадь квадрата со стороной  " + var + " =  " + square(var) );
+        System.out.println("Площадь прямоугольника со сторонами " + l + " и" + " " + s + " = " + longSquare(l,s) );
+    }
+
+    public static void hello(String w){
+        System.out.println("hello " + w);
+    }
+
+    public static double square(double l){
+        return l * l;
+    }
+
+    public static double longSquare(double l, double s){
+        return l * s;
     }
 }
